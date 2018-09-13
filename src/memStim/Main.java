9044c041f48a6b1e0e6d13c10bcd66e37b91f3ea
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-13 21:54:29 +0200

    Made width and height independent and dependent on the mainPane size

--- src/memStim/Main.java
+++ src/memStim/Main.java
@@ -13,18 +13,23 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         AnchorPane MainPane = new AnchorPane();
-        int rows = 8;
-        int columns = 8;
+        int rows = 5;
+        int columns = 5;
+        MainPane.setPrefWidth(600);
+        MainPane.setPrefHeight(600);
+        double height = MainPane.getPrefHeight();
+        double width = MainPane.getPrefWidth();
         for (int i = 0; i<rows; i++) {
             for (int j=0; j<columns; j++) {
                 Rectangle square = new Rectangle();
-                double var = 600.0/rows;
-                square.setHeight(var);
-                square.setWidth(var);
-                MainPane.setTopAnchor(square, var*i);
-                MainPane.setBottomAnchor(square, var*(rows-i));
-                MainPane.setRightAnchor(square, var*(columns-j));
-                MainPane.setLeftAnchor(square, var*j);
+                double sqHeight = height/rows;
+                double sqWidth = width/columns;
+                square.setHeight(sqHeight);
+                square.setWidth(sqWidth);
+                MainPane.setTopAnchor(square, sqHeight*i);
+                MainPane.setBottomAnchor(square, sqHeight*(rows-i));
+                MainPane.setRightAnchor(square, sqWidth*(columns-j));
+                MainPane.setLeftAnchor(square, sqWidth*j);
                 if ((i+j)%2==0) {
                     square.setFill (Color.BLACK);
                 }else{
