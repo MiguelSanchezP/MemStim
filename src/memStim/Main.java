d8b36a696d037bcaa825bce17a412d269f2b5ef8
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-15 22:03:01 +0200

    made the main open the mainWindow.fxml

--- src/memStim/Main.java
+++ src/memStim/Main.java
@@ -1,45 +1,18 @@
 package memStim;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        AnchorPane MainPane = new AnchorPane();
-        int rows = 5;
-        int columns = 5;
-        MainPane.setPrefWidth(600);
-        MainPane.setPrefHeight(600);
-        double height = MainPane.getPrefHeight();
-        double width = MainPane.getPrefWidth();
-        for (int i = 0; i<rows; i++) {
-            for (int j=0; j<columns; j++) {
-                Rectangle square = new Rectangle();
-                double sqHeight = height/rows;
-                double sqWidth = width/columns;
-                square.setHeight(sqHeight);
-                square.setWidth(sqWidth);
-                MainPane.setTopAnchor(square, sqHeight*i);
-                MainPane.setBottomAnchor(square, sqHeight*(rows-i));
-                MainPane.setRightAnchor(square, sqWidth*(columns-j));
-                MainPane.setLeftAnchor(square, sqWidth*j);
-                if ((i+j)%2==0) {
-                    square.setFill (Color.BLACK);
-                }else{
-                    square.setFill (Color.WHITE);
-                }
-                MainPane.getChildren().add(square);
-            }
-        }
-        Scene scene = new Scene (MainPane, 600, 600);
-        primaryStage.setTitle("MemStim");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start (Stage stage) throws Exception {
+        stage.setTitle("MemStim");
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        stage.setScene(new Scene (root, 600, 600));
+        stage.show();
     }
 
     public static void main (String[] args) {
