7e95df98e8a9197cdfc28ac9cfb9a2b6ee4d16d6
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-26 21:28:52 +0200

    added the case to the handleNewGame

--- src/memStim/MainWindowController.java
+++ src/memStim/MainWindowController.java
@@ -1,8 +1,12 @@
 package memStim;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class MainWindowController {
     /*
@@ -39,8 +43,8 @@ public class MainWindowController {
 
     @FXML
     private AnchorPane APMainWindow;
-    @FXML
-    private Button btn1, btn2;
+
+    private Stage primaryStage = new Stage();
 
     public void initialize () {
 
@@ -48,13 +52,36 @@ public class MainWindowController {
 
     @FXML
     private void handleNewGameBtn () {
-        if (btn1.isVisible()) {
-            btn1.setVisible(false);
-            btn2.setVisible(false);
-        } else {
-            btn1.setVisible(true);
-            btn2.setVisible(true);
+        AnchorPane MainPane = new AnchorPane();
+        int rows = 5;
+        int columns = 5;
+        MainPane.setPrefWidth(600);
+        MainPane.setPrefHeight(600);
+        double height = MainPane.getPrefHeight();
+        double width = MainPane.getPrefWidth();
+        for (int i = 0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                Rectangle square = new Rectangle();
+                double sqHeight = height/rows;
+                double sqWidth = width/columns;
+                square.setHeight(sqHeight);
+                square.setWidth(sqWidth);
+                MainPane.setTopAnchor(square, sqHeight*i);
+                MainPane.setBottomAnchor(square, sqHeight*(rows-i));
+                MainPane.setRightAnchor(square, sqWidth*(columns-j));
+                MainPane.setLeftAnchor(square, sqWidth*j);
+                if ((i+j)%2==0) {
+                    square.setFill (Color.BLACK);
+                }else{
+                    square.setFill (Color.WHITE);
+                }
+                MainPane.getChildren().add(square);
+            }
         }
+        Scene scene = new Scene (MainPane, 600, 600);
+        primaryStage.setTitle("MemStim");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
 }
