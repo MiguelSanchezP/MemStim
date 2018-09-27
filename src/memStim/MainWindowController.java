f0e24ee28f2bc9f07c8cf44bd7715dbe24d2d0cb
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-27 22:58:16 +0200

    removed useless comments and imports

--- src/memStim/MainWindowController.java
+++ src/memStim/MainWindowController.java
@@ -2,63 +2,20 @@ package memStim;
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.util.Random;
-
 public class MainWindowController {
-    /*
-    * AnchorPane MainPane = new AnchorPane();
-        int rows = 5;
-        int columns = 5;
-        MainPane.setPrefWidth(600);
-        MainPane.setPrefHeight(600);
-        double height = MainPane.getPrefHeight();
-        double width = MainPane.getPrefWidth();
-        for (int i = 0; i<rows; i++) {
-            for (int j=0; j<columns; j++) {
-                Rectangle square = new Rectangle();
-                double sqHeight = height/rows;
-                double sqWidth = width/columns;
-                square.setHeight(sqHeight);
-                square.setWidth(sqWidth);
-                MainPane.setTopAnchor(square, sqHeight*i);
-                MainPane.setBottomAnchor(square, sqHeight*(rows-i));
-                MainPane.setRightAnchor(square, sqWidth*(columns-j));
-                MainPane.setLeftAnchor(square, sqWidth*j);
-                if ((i+j)%2==0) {
-                    square.setFill (Color.BLACK);
-                }else{
-                    square.setFill (Color.WHITE);
-                }
-                MainPane.getChildren().add(square);
-            }
-        }
-        Scene scene = new Scene (MainPane, 600, 600);
-        primaryStage.setTitle("MemStim");
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
-
-    @FXML
-    private AnchorPane APMainWindow;
 
     private Stage primaryStage = new Stage();
-
-    public void initialize () {
-
-    }
 
     @FXML
     private void handleNewGameBtn () {
         AnchorPane MainPane = new AnchorPane();
         int rows = 5;
         int columns = 5;
-        boolean coloured [];
         MainPane.setPrefWidth(600);
         MainPane.setPrefHeight(600);
         double height = MainPane.getPrefHeight();
@@ -75,11 +32,6 @@ public class MainWindowController {
                 MainPane.setBottomAnchor(square, sqHeight*(rows-i));
                 MainPane.setRightAnchor(square, sqWidth*(columns-j));
                 MainPane.setLeftAnchor(square, sqWidth*j);
-//                if ((i+j)%2==0) {
-//                    square.setFill (Color.BLACK);
-//                }else{
-//                    square.setFill (Color.WHITE);
-//                }
                 MainPane.getChildren().add(square);
                 if (random == 0) {
                     square.setFill (Color.BLACK);
