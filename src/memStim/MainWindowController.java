507e959937f4a9866b427a0a47b1425307762138
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-30 15:46:04 +0200

    added delay method and comments to future improvements

--- src/memStim/MainWindowController.java
+++ src/memStim/MainWindowController.java
@@ -10,14 +10,18 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainWindowController {
 
     private Stage primaryStage = new Stage();
     private double x, y;
+    private HashMap<Integer[], Boolean> configurations = new HashMap<>();
 
     @FXML
-    private void handleNewGameBtn() {
+    private void handleNewGameBtn() throws InterruptedException {
         GridPane MainPane = new GridPane();
         int rows = 5;
         int columns = 5;
@@ -27,20 +31,23 @@ public class MainWindowController {
         double width = MainPane.getPrefWidth();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                int random = (int) (Math.random() * 2);
+                int random = (int) (Math.random() * 4);
                 Rectangle square = new Rectangle();
                 double sqHeight = height / rows;
                 double sqWidth = width / columns;
                 square.setHeight(sqHeight);
                 square.setWidth(sqWidth);
+                MainPane.getChildren().add(square);
                 MainPane.setColumnIndex(square, j);
                 MainPane.setRowIndex(square, i);
-                MainPane.getChildren().add(square);
                 square.setStroke(Color.GREY);
-                if (random == 0) {
+                Integer[] numbers = {i, j};
+                if (random<1) {
                     square.setFill(Color.BLACK);
+                    configurations.put(numbers, true);
                 } else {
                     square.setFill(Color.WHITE);
+                    configurations.put(numbers, false);
                 }
             }
         }
@@ -49,20 +56,34 @@ public class MainWindowController {
             public void handle(MouseEvent e) {
                 x = e.getX();
                 y = e.getY();
-                System.out.println("The mouse was clicked at the point (" + x + "," + y + ")");
+                //add the method for the change color. add boolean if possible
             }
         });
         Scene scene = new Scene(MainPane, 600, 600);
         primaryStage.setTitle("MemStim");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //create the code to clean the gridpane and to make the application change with the clicked mouse
+            }
+        }, 2000);
     }
 
-    private Node getNodeWithColumnAndIndex(GridPane gp, int column, int row) {
+    private void updatePane(GridPane gp) throws InterruptedException{
+        Thread.sleep(5000);
+        gp.getChildren().clear();
+    }
+
+    private Rectangle getNodeWithColumnAndIndex(GridPane gp, int column, int row) {
         ObservableList<Node> list = gp.getChildren();
         for (Node n : list) {
-            if (gp.getRowIndex(n) == row && gp.getColumnIndex(n) == column) {
-                return n;
+            if (gp.getRowIndex(n).equals(row) && gp.getColumnIndex(n).equals(column)) {
+                return(Rectangle) n;
             }
         }
         return null;
