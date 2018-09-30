9e102441a6769447ee9b254d3c9d617cdcaf89bc
miguelsanchez2003 <miguelsanchez1703@gmail.com>
2018-09-30 13:11:37 +0200

    added the onclicked event for the game

--- src/memStim/MainWindowController.java
+++ src/memStim/MainWindowController.java
@@ -1,49 +1,84 @@
 package memStim;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+//import java.awt.event.MouseEvent;
+import javafx.event.Event;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
+
 public class MainWindowController {
 
     private Stage primaryStage = new Stage();
+    private double x, y;
 
     @FXML
-    private void handleNewGameBtn () {
-        AnchorPane MainPane = new AnchorPane();
+    private void handleNewGameBtn() {
+        GridPane MainPane = new GridPane();
         int rows = 5;
         int columns = 5;
         MainPane.setPrefWidth(600);
         MainPane.setPrefHeight(600);
         double height = MainPane.getPrefHeight();
         double width = MainPane.getPrefWidth();
-        for (int i = 0; i<rows; i++) {
-            for (int j=0; j<columns; j++) {
-                int random = (int)(Math.random()*2);
+        int number = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                int random = (int) (Math.random() * 2);
                 Rectangle square = new Rectangle();
-                double sqHeight = height/rows;
-                double sqWidth = width/columns;
+                double sqHeight = height / rows;
+                double sqWidth = width / columns;
                 square.setHeight(sqHeight);
                 square.setWidth(sqWidth);
-                MainPane.setTopAnchor(square, sqHeight*i);
-                MainPane.setBottomAnchor(square, sqHeight*(rows-i));
-                MainPane.setRightAnchor(square, sqWidth*(columns-j));
-                MainPane.setLeftAnchor(square, sqWidth*j);
+                MainPane.setColumnIndex(square, j);
+                MainPane.setRowIndex(square, i);
                 MainPane.getChildren().add(square);
+                square.setStroke(Color.GREY);
                 if (random == 0) {
-                    square.setFill (Color.BLACK);
-                }else{
+                    square.setFill(Color.BLACK);
+                } else {
                     square.setFill(Color.WHITE);
                 }
             }
         }
-        Scene scene = new Scene (MainPane, 600, 600);
+        MainPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                x = e.getX();
+                y = e.getY();
+                System.out.println("The mouse was clicked at the point (" + x + "," + y + ")");
+            }
+        });
+        Scene scene = new Scene(MainPane, 600, 600);
         primaryStage.setTitle("MemStim");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private Node getNodeWithColumnAndIndex(GridPane gp, int column, int row) {
+        ObservableList<Node> list = gp.getChildren();
+        for (Node n : list) {
+            if (gp.getRowIndex(n) == row && gp.getColumnIndex(n) == column) {
+                return n;
+            }
+        }
+        return null;
+    }
 }
